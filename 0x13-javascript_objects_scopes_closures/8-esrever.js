c697576e1d21107ff3cfb3e83ab7af878a9aea3e
ItsZeusX <83469994+ItsZeusX@users.noreply.github.com>
2023-09-29 15:00:35 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-  const newList = [];
-  for (let i = list.length, j = 0; i > 0; i--, j++) {
-    newList[j] = list[i - 1];
-  }
-  return newList;
-};
